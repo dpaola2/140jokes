066830392d7ad9751400b464f3ab56aa3b5b2740
Dave Paola <dpaola2@gmail.com>
2011-02-03 01:17:34 -0800

    added newer version of compojure

--- jokes/project.clj
+++ jokes/project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [ring/ring-jetty-adapter "0.3.1"]
-                 [compojure "0.5.3"]
+                 [compojure "0.6.0-RC3"]
                  [hiccup "0.3.2"]
                  [digest "1.0.0-SNAPSHOT"]
                  [redis-clojure "1.0.4"]]
